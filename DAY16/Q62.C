46c3e47005ca25ecbad2651539d8a383dd3294d7
GLB-PURNENDRA <www.shyamyt@gmail.com>
2026-06-19 20:20:09 +0530

    Added Day 16 to Day 19 programs

---
+++ DAY16/Q62.C
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int maxCount = 0, maxElement;
+
+    for(int i = 0; i < n; i++)
+    {
+        int count = 1;
+
+        for(int j = i + 1; j < n; j++)
+        {
+            if(arr[i] == arr[j])
+                count++;
+        }
+
+        if(count > maxCount)
+        {
+            maxCount = count;
+            maxElement = arr[i];
+        }
+    }
+
+    printf("Maximum Frequency Element = %d", maxElement);
+
+    return 0;
+}
